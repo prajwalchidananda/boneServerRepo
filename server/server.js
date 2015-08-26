5503a2407d1f706ec2df3812aeed1e27855f6b36
prajwalchidananda <prajwal.chidanand@gmail.com>
2015-08-26 09:32:53 +0000

    Commiting project

--- server/server.js
+++ server/server.js
@@ -1,11 +1,42 @@
-var test;
+var express = require('express');
+var app = express(); 
+//var MongoClient = require("mongodb").MongoClient;
 
-var test2;
 
-var test3;
 
-<<<<<<< HEAD
-var test5;
-=======
-var test4;
->>>>>>> origin/master
+app.configure(
+function()
+{
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+}
+);
+
+app.get('/boneGet', 
+	function(req,res)
+	{
+		/*Selecting all items from the seven wonders database
+		Not displaying the id field. converting to array */ 
+		// var items = db.SevenWonders.find({}, {_id : 0}).toArray();
+		 var items = [{name:'Giza Necropolis', location: 'Giza, Eqypt'},
+		 	{name:'Great Wall of China', location: 'China'},
+		 	{name:'Petra', location: 'Maan Governorate, Jordan'},
+		 	{name: 'Colosseum', location: 'Rome, Italy'},
+		 	{name: 'Chichen Itza', location: 'Yucatan, Mexico' },
+		 	{name: 'Machu Picchu', location: 'Cuzco, Peru'},
+		 	{name: 'Taj Mahal', location:'Agra, Uttar Pradesh, India'},
+		 	{name: 'Christ the Redeemer', location: 'Rio de Janeiro, Brazil'}];
+		res.send(items);
+	}
+);
+
+app.post('/bonePost', function (req, res) {
+  
+  res.send('Got a POST request');
+  console.log(req.body);
+});
+
+
+
+app.listen(process.env.PORT);
+console.log('Listening on process.env.PORT');
